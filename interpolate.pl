3792ab7a520de1de1adfd2c7f42e9ef59cf6d8d6
Robin Smith <robin@mrrobinsmith.com>
2014-04-17 18:05:17 -0700

    Auto-update

---
+++ interpolate.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $place = "World";
+print "Hello $place!\n";
---
+++ reg_exp.pl
@@ -0,0 +1,4 @@
+string="Bill Clinton"
+
+if($string =~ m/(B|b)ill (C|c)linton/)
+  {print "It is Clinton, all right!\n"}
