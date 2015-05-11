48c422eb475d06dc5ca6d36a2c1df4ff5627f93e
Robin Smith <robin@mrrobinsmith.com>
2015-05-11 16:08:37 +0100

    Auto-update.

---
+++ practice/pipe.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+# This is out to take piped-in input
+while (<>) {
+  print $_
+}
---
+++ practice/regex/doubled_words.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+
+$/ = ".\n";
+while (<>) {
+  # next line if no doubled words found
+  next if !s/\b([a-z]+)((?:\s|<[^>]+>)+)(\1\b)/\e[7m$1\e[m$2\e[7m$3\e[m/ig;
+  s/^(?:[^\e]*\n)+//mg;   # Remove any unmarked lines.
+  s/^/$ARGV: /mg;         # Ensure lines begin with filename.
+  print;
+}
