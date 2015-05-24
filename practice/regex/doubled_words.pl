b5818d9dba9cfcdef0e4250eae1f38e8fb276abc
Robin Smith <robin@mrrobinsmith.com>
2015-05-24 22:26:03 +0100

    Auto-update.

--- practice/regex/doubled_words.pl
+++ practice/regex/doubled_words.pl
@@ -4,11 +4,19 @@ use strict;
 use warnings;
 
 
+# This makes <> return paragraph-sized chunks instead of
+# single lines
 $/ = ".\n";
+
 while (<>) {
-  # next line if no doubled words found
-  next if !s/\b([a-z]+)((?:\s|<[^>]+>)+)(\1\b)/\e[7m$1\e[m$2\e[7m$3\e[m/ig;
-  s/^(?:[^\e]*\n)+//mg;   # Remove any unmarked lines.
-  s/^/$ARGV: /mg;         # Ensure lines begin with filename.
+  next unless s{
+    \b([a-z]+)((?:\s<<[^>]+>)+)(\1\b)
+  }{
+    \e[7m$1\e[m$2\e[7m$3\e[m
+  }igx;
+
+  # s/^(?:[^\e]*\n)+//mg;   # Remove any unmarked lines.
+  # s/^/$ARGV: /mg;         # Ensure lines begin with filename.
+
   print;
 }
--- practice/regex/text_to_html.pl
+++ practice/regex/text_to_html.pl
@@ -18,16 +18,45 @@ $text =~ s/>/&gt;/g;
 # is therefore two newlines in a row: \n\n
 $text =~ s/^\s*$/<p>/mg;
 
+
+# This will be used multiple timeso
+# This introduces Perl’s qr operator. It’s similar to the m and s operators
+# in that it takes a regular expression
+# (i.e., used as qr/···/, just like m/···/ and s/···/···/), but rather than
+# immediately applying it to some text in search of a match, it converts the 
+# regex provided into a regex object, which you can save to a variable.
+#
+# ####################################################################
+# This makes Perl a good language to use for building a regex library!
+# ####################################################################
+my $HostnameRegex = qr/[-a-z0-9]+(\.[-a-z0-9]+)*\.(com|edu|info)/i;
+
+
 # 'Linkify' email addresses
+# /i option performs case-insensitive search
+# /x means ignore whitespace in the regex itself - so that it can be
+# formatted nicely over multiple lines
 $text =~ s{
   \b
     # Capture the address to $1
     (
       \w[-.\w]* # username
       \@
-      [-a-z0-9]+(\.[-a-z0-9]+)*\.(com|edu|info) # hostname
+     $HostnameRegex
     )
   \b
 }{<a href="mailto:$1">$1<\/a>}gix;
+
+# 'Linkify' http URLs
+$text =~ s{
+  (
+    http://$HostnameRegex
+    (
+      # Note the escaped Perl interpolation symbols @ and $
+      [-a-z0-9_:\@&?=+,.!/~*'%\$]+ # optional path
+    )?
+    (?<![,])
+  )
+}{<a href="$1">Link!</a>}gix; 
 
 print "$text\n";
